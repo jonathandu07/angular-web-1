794d9c4a0d7100e5ad227d9711b29786f24261da
jonathan YHWH <alphajorider@gmail.com>
2022-04-08 12:28:49 +0200

    vendredi
    vendredi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,32 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  title = "home"
+
+  arrayImg = ['auto-2179220.jpg','bullets-2166491.jpg','car-49278.jpg','glock-2424292.jpg','soldier-1447008.jpg'];
+
+  randomIndex = Math.floor(Math.random()*5);
+
+  sourceImg = this.arrayImg[this.randomIndex];
+
+  compteur = 0;
+  Myalert(msg:string){
+    alert(msg);
+  }
+
+  changeImage(){
+    this.randomIndex = Math.floor(Math.random()*5);
+    this.sourceImg = this.arrayImg[this.randomIndex];
+    this.compteur++;
+    console.log('Nombre de changement de source',this.compteur);
+
+
+  }
+
   ngOnInit(): void {
+    console.log(this.sourceImg);
+    this.changeImage();
+
   }
 
 }
