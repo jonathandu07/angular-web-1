c7636636ea2257469eb0cefc513036192e2f9856
jonathan YHWH <alphajorider@gmail.com>
2022-04-19 11:31:17 +0200

    mise à jour 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,38 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
 
-  title = "home"
+  title = 'home';
 
-  arrayImg = ['auto-2179220.jpg','bullets-2166491.jpg','car-49278.jpg','glock-2424292.jpg','soldier-1447008.jpg'];
+  arrayImg = [
+    'auto-2179220.jpg',
+    'bullets-2166491.jpg',
+    'car-49278.jpg',
+    'glock-2424292.jpg',
+    'soldier-1447008.jpg',
+  ];
 
-  randomIndex = Math.floor(Math.random()*5);
+  JsonImg = [
+    { url: 'auto-2179220.jpg', alt: 'voiture' },
+    { url: 'bullets-2166491.jpg', alt: 'balle' },
+    { url: 'car-49278.jpg', alt: 'bugatti' },
+    { url: 'glock-2424292.jpg', alt: 'glock' },
+    { url: 'soldier-1447008.jpg', alt: 'soldat' },
+  ];
+
+  randomIndex = Math.floor(Math.random() * 5);
 
   sourceImg = this.arrayImg[this.randomIndex];
 
   compteur = 0;
-  Myalert(msg:string){
+  Myalert(msg: string) {
     alert(msg);
   }
 
-  changeImage(){
-    this.randomIndex = Math.floor(Math.random()*5);
+  changeImage() {
+    this.randomIndex = Math.floor(Math.random() * 5);
     this.sourceImg = this.arrayImg[this.randomIndex];
     this.compteur++;
-    console.log('Nombre de changement de source',this.compteur);
-
-
+    console.log('Nombre de changement de source', this.compteur);
   }
+
+  myColor = '#282E69';
 
   ngOnInit(): void {
     console.log(this.sourceImg);
     this.changeImage();
-
   }
-
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+// import { url } from 'inspector';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
@@ -7,9 +8,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
-  ngOnInit(): void {
+  joke:any;
+  JokeNoFound=true;
+
+  gallery:any;
+  page = 1;
+  urlPicsum='https://picsum.photos/v2/list?page='+this.page+'&limit=6'
+
+  nextPage(){
+    this.page = this.page +1;
+    this.urlPicsum='https://picsum.photos/v2/list?page='+this.page+'&limit=6'
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
   }
 
-}
+  backPage(){
+    this.page ? this.page -- : null;
+    this.urlPicsum='https://picsum.photos/v2/list?page='+this.page+'&limit=6'
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+
+  Page1(){
+    this.page = this.page =1;
+    this.urlPicsum='https://picsum.photos/v2/list?page='+this.page+'&limit=6'
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+
+  Page2(){
+    this.page = this.page =2;
+    this.urlPicsum='https://picsum.photos/v2/list?page='+this.page+'&limit=6'
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+
+  Page3(){
+    this.page = this.page =3;
+    this.urlPicsum='https://picsum.photos/v2/list?page='+this.page+'&limit=6'
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+  getUrl(url:string){
+    return this.http.get(url);
+  }
+
+  loadPics(){
+    this.getUrl(this.urlPicsum).subscribe(
+      (data)=>{
+        this.gallery=data;
+        console.log(data);
+  })}
+
+  ngOnInit(): void {
+    this.http.get('https://api.chucknorris.io/jokes/random').subscribe(
+      (data)=>{
+        this.joke=data;
+        this.JokeNoFound = false;
+        console.log(data);
+      }
+    )
+
+    this.loadPics();
+      }
+    
+  }
+
