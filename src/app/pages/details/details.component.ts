77439ca53a50558fec212a90848fe051ea236eb5
jonathan YHWH <alphajorider@gmail.com>
2022-04-20 10:40:42 +0200

    mise à jour

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -14,6 +14,33 @@ export class DetailsComponent implements OnInit {
   constructor(public http:HttpClient,private route:ActivatedRoute) { }
   itemId = '';
   itemInfo:any;
+  source='';
+  griser = '&grayscale';
+  niveau = 1;
+  flou = 'blur='+this.niveau;
+  grisFlou = this.griser += this.flou;
+  gris(){
+    this.source +=this.griser;
+  }
+
+  floutage(){
+    this.source +=this.flou;
+  }
+
+  plus(){
+    this.source +this.flou+this.niveau++;
+    console.log(this.source);
+  }
+
+  moins(){
+    this.niveau >1 ?this.niveau--: null;
+    this.source += this.niveau;
+    console.log(this.source);
+  }
+
+  flouGris(){
+    this.source += this.grisFlou;
+  }
 
   ngOnInit(): void {
     // on récupère l'id dans la route active
@@ -21,6 +48,7 @@ export class DetailsComponent implements OnInit {
     console.log('itemId',this.itemId);
     // on charge les données correspondantes de l'image
     this.http.get('https://picsum.photos/id/' +this.itemId+'/info').subscribe(data =>{ this.itemInfo=data;console.log(data);
+    this.source = 'https://picsum.photos/id/'+this.itemId+'/1280/960?';
   })
   }
 }
